5d2de02681f654adc12da08f99db3a8b1a1ac5f1
Danniel <dannielaguswahyudi@gmail.com>
2020-10-08 09:46:23 +0700

    .

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.550acf1b8736fa727e2288ea995ca507.js"
+  "/precache-manifest.49dc210b9c319e1dab347838ac6282a3.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "theme-one"});
