8ca1336e4947fb21cd7071d81050ec9372fb44c5
myNameisAkmal <zdiezzystark@gmail.com>
2021-02-18 23:00:21 +0700

    akmal

--- 1003/service-worker.js
+++ 1003/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ac51f956cdec60aedcfde35ebb236635.js"
+  "/precache-manifest.62c2782a69362a6421bd06a4c921f67d.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "theme-one"});
